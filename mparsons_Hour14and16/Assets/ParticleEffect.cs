f1804c2d699aeaefd64e2327e8a4d5183044944f
Uηιχιту <miloparsons10@gmail.com>
2026-05-17 19:07:37 -0700

    Hour 16 done

---
+++ mparsons_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+#1. Made the particles blue and smaller
+#2. Added gravity to the particles
+#3. Added a raindrop texture to the particles
+#4. Made the particles last longer
+#5. Added cubes for the raindrops to collide with
+#6. Added more raindrops to create a heavier rain effect
+*/
